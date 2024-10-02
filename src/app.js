013fcbe9db66dbf2c09b5e0003a44cc1eac08922
SrAlbert0 <73313619+SrAlbert0@users.noreply.github.com>
2024-10-02 19:28:04 +0000

    One result

--- src/app.js
+++ src/app.js
@@ -8,4 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  const who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "my phone", "the car"];
+  const when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const argExGen = arr => arr[Math.floor(Math.random() * arr.length)];
+
+  console.log(
+    argExGen(who) +
+      " " +
+      argExGen(action) +
+      " " +
+      argExGen(what) +
+      " " +
+      argExGen(when)
+  );
 };
